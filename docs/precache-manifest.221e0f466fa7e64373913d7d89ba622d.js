cd89b4699b3258717af08faf897bef868e11a7e3
Victor Jaramillo <v87jara@hotmail.com>
2020-10-15 22:18:56 -0500

    Actualizacion para produccion

---
+++ docs/precache-manifest.221e0f466fa7e64373913d7d89ba622d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d21dd571a6eb64be55bf31ed5efde80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d50748daf6978c1a6eff",
+    "url": "/static/css/main.c2528d82.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d50748daf6978c1a6eff",
+    "url": "/static/js/main.03187205.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
